db605ec8ab0eea8c5ee1aa8474c8d28ee5cd1acf
Adeolu Alagbe <alagbeade@gmail.com>
2023-06-10 21:14:51 +0100

    added error handling

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
-import React, {useState}from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies,setMovies] = useState([])
+  const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  const fetchMoviesHandler = async() => {
+  const fetchMoviesHandler = async () => {
     setIsLoading(true)
+    setError(null)
 
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json() //An alternative use of async await instead of using then
-    const transformedMovies = data.results.map(movieData => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          }
-        })
-    // .then(response => {
-    //   return response.json()
-    // })
-    // .then(data => {
-    //   const transformedMovies = data.results.map(movieData => {
-    //     return {
-    //       id: movieData.episode_id,
-    //       title: movieData.title,
-    //       openingText: movieData.opening_crawl,
-    //       releaseDate: movieData.release_date
-    //     }
-    //   })
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+      if (!response.ok) {
+        throw new Error('Something went wrong')
+      }
+      const data = await response.json() //An alternative use of async await instead of using then
+      
+
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      // .then(response => {
+      //   return response.json()
+      // })
+      // .then(data => {
+      //   const transformedMovies = data.results.map(movieData => {
+      //     return {
+      //       id: movieData.episode_id,
+      //       title: movieData.title,
+      //       openingText: movieData.opening_crawl,
+      //       releaseDate: movieData.release_date
+      //     }
+      //   })
       setMovies(transformedMovies)
-      setIsLoading(false)
+    } catch (error) {
+      setError(error.message)
     }
-  
+    setIsLoading(false)
+  }
+
 
   return (
     <React.Fragment>
@@ -44,8 +55,10 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies</p> }
+        {!isLoading && !error && movies.length === 0 && <p>Found no movies</p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
+        
       </section>
     </React.Fragment>
   );
